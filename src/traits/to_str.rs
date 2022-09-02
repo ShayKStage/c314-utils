a682fbaab3c49da7e5d0232b99774dd452b420d1
314ShadePi <erikkirkegaardstage@gmail.com>
2022-09-02 10:09:48 +0200

    Add documentation to src/traits/to_str.rs

--- src/traits/to_str.rs
+++ src/traits/to_str.rs
@@ -1,8 +1,12 @@
+/// trait ToStr containing function/method to_str that is used tp convert between a string and a static str
 pub trait ToStr {
+    /// Function/Method to_str takes in a string and returns a static str
     fn to_str(self) -> &'static str;
 }
 
+/// implementation of ToStr for String
 impl ToStr for String {
+    /// Implementation of to_str using a Box and leaking it
     fn to_str(self) -> &'static str {
         Box::leak(self.into_boxed_str())
     }
